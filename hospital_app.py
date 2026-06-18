1f5a4d7ded9a984c112461250d7fb7ba6795156d
atmadjamichael325-gif <atmadjamichael325@gmail.com>
2026-06-18 11:49:52 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -30,30 +30,95 @@ DEPT_INFO = {
     'color': '#035afc',
     'desc: "Specializes in conditions affecting the lungs and airways.',
     'step': ['Visit level 2, Wing B', 'Estimated wait: 15-25 min', 'please wear mask'],
-},
-'Cardiology': {
+  },
+  'Cardiology': {
         'icon': '❤️', 'color': '#dc2626',
         'desc': 'Specialises in heart and cardiovascular conditions.',
         'steps': ['Visit Level 3, Wing A', 'Estimated wait: 20–30 min', 'Bring any previous ECG reports'],
-},
-'Gastroenterology': {
+  },
+  'Gastroenterology': {
         'icon': '🫃', 'color': '#d97706',
         'desc': 'Specialises in digestive system and abdominal conditions.',
         'steps': ['Visit Level 1, Wing C', 'Estimated wait: 10–20 min', 'Avoid eating before consultation'],
-},
-'Neurology': {
+  },
+  'Neurology': {
         'icon': '🧠', 'color': '#7c3aed',
         'desc': 'Specialises in brain, spine, and nervous system conditions.',
         'steps': ['Visit Level 4, Wing A', 'Estimated wait: 25–35 min', 'Bring list of current medications'],
-},
-'General Medicine': {
+  },
+  'General Medicine': {
         'icon': '🩺', 'color': '#059669',
         'desc': 'Handles general health concerns and non-specialist conditions.',
         'steps': ['Visit Level 1, Wing A', 'Estimated wait: 10–15 min', 'Registration desk is open 24/7'],
-},
+  },
     'Dermatology': {
         'icon': '🔬', 'color': '#b45309',
         'desc': 'Specialises in skin, hair, and nail conditions.',
         'steps': ['Visit Level 2, Wing D', 'Estimated wait: 15–20 min', 'Bring photos of affected area if possible'],
-},
+  },
 }
+
+
+
+def predict_department(inputs: dict) -> tuple[str, float, list]:
+  patient_df = pd.DataFrame([inputs])
+  patient_df[cols_to_scale] = scaler.transform(patient_df[cols_to_scale])
+  # Run the Model
+  predicted_class = model.predict(patient_df[features])[0]
+  all_proba = model.predict_proba(patient_df[features])[0]
+
+  # Conver Number to human readable label
+  dept_name = dept_map_inv[predicted_class]
+  confidence = all_proba[predicted_class] * 100
+
+  return dept_name, confidence, all_proba
+
+def show_header():
+  st.markdown("""
+    <div style=background: linear-gradient(135deg, #1e3a8a, #1a56db, #0ea5e9);
+               padding :3rem 2rem: text-align: center; margin-bottom: 2 rem;">
+        <p style="color: #ededed; font-size: 13px;>
+            FUTURE CLASSROOM: Machine Learning
+        </p>
+        <h1>
+          Smart Hospital Patient Navigator
+        </h1>
+        <p style=color: white; font-size: 18px;>
+            Find the right department for your symptoms
+        </p>
+      </div>
+  """, unsafe_allow_html=True)
+  def show_symptom_section():
+    """Returns a dict of symptom checkboxes {symptom_name: True/False}"""
+    st.subheader("1. What are your main symptoms?")
+
+    col1, col2, col3, col4 = st.columns(4)
+    symptoms = {}
+
+    with col1:
+        symptoms['fever'] = st.checkbox("🌡️ Fever")
+        symptoms['cough'] = st.checkbox("🤧 Cough")
+    with col2:
+        symptoms['headache']   = st.checkbox("🤕 Headache")
+        symptoms['chest_pain'] = st.checkbox("💔 Chest Pain")
+    with col3:
+        symptoms['stomach_pain']     = st.checkbox("🤢 Stomach Pain")
+        symptoms['shortness_breath'] = st.checkbox("😮‍💨 Shortness of Breath")
+    with col4:
+        symptoms['nausea_vomiting'] = st.checkbox("🤮 Nausea / Vomiting")
+        symptoms['dizziness']       = st.checkbox("😵 Dizziness")
+
+    col5, _, _, _ = st.columns(4)
+    with col5:
+        symptoms['skin_rash'] = st.checkbox("🔴 Skin Rash")
+
+    return symptoms
+    
+def main():
+    st.set_page_config(page_title="Smart Hospital Patient Navigator", page_icon="🏥", layout="wide")
+
+    show_header()
+
+if __name__ == "__main__":
+    main()
+
