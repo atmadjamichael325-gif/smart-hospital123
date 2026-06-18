35cb4f9d182566a325474d901beeef9968821a5e
atmadjamichael325-gif <atmadjamichael325@gmail.com>
2026-06-18 12:03:02 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -6,7 +6,7 @@ import pickle
 
 @st.cache_resource
 def load_model():
-  with open('hospital_model.pkl', 'rb') as f:
+  with open('hospital_model (1).pkl', 'rb') as f:
     return pickle.load(f)
 
 bundle = load_model()
